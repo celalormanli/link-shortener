217843ec0d73b12c74178d277a84ccad6fed988d
Celal ORMANLI <ormanlicelal@gmail.com>
2024-12-31 00:46:14 +0300

    add redis for caching

--- link/views.py
+++ link/views.py
@@ -3,13 +3,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_api_key.models import APIKey
 from rest_framework_api_key.permissions import HasAPIKey
-from django.http import HttpResponsePermanentRedirect
+from django.http import HttpResponsePermanentRedirect, HttpResponseNotFound
 from django.db.models import F
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.core.cache import cache
 
 from link.models import Link
 from link.throttling import CreateShortLinkThrottle
 from link.serializers import LinkSerializer
 import uuid
+
 
 class LinkViewSet(viewsets.ViewSet):
     permission_classes = [HasAPIKey]
@@ -29,14 +33,28 @@ class LinkViewSet(viewsets.ViewSet):
     def list(self, request):
         key = request.META["HTTP_AUTHORIZATION"].split()[1]
         api_key = APIKey.objects.get_from_key(key)
-        list_of_links = Link.objects.filter(api_key = api_key)
+        list_of_links = Link.objects.filter(api_key=api_key)
         serializer = LinkSerializer(list_of_links, many=True)
         return Response(serializer.data)
 
+
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+
 class LinkRedirectViewSet(APIView):
     permission_classes = []
-    
+
     def get(self, request, *args, **kwargs):
-        link = Link.objects.filter(shorted_link=kwargs["shorted_link"]).first()
-        Link.objects.filter(shorted_link=kwargs["shorted_link"]).update(redirect_counter=F("redirect_counter") + 1)
-        return HttpResponsePermanentRedirect(link.main_link)
+        main_link = None
+        if kwargs["shorted_link"] in cache:
+            main_link = cache.get(kwargs["shorted_link"])
+        else:
+            link = Link.objects.filter(
+                shorted_link=kwargs["shorted_link"]).first()
+            main_link = link.main_link
+            cache.set(kwargs["shorted_link"], main_link, timeout=CACHE_TTL)
+        Link.objects.filter(shorted_link=kwargs["shorted_link"]).update(
+            redirect_counter=F("redirect_counter") + 1)
+        
+        return HttpResponsePermanentRedirect(main_link)
+        
--- linkshortener/settings.py
+++ linkshortener/settings.py
@@ -141,3 +141,13 @@ REST_FRAMEWORK = {
         "create_short_link" : "50/day",
     }, 
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        }
+    }
+}
